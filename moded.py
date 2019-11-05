b6bec2430113c90c5a144830fd6c894a9ddde348
femanov <femanov@gmail.com>
2019-11-05 15:38:14 +0700

    initial working version with pg11 and JSON

--- moded.py
+++ moded.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-
 import sys
 import time
 from math import fabs
 import numpy as np
+import simplejson as json
 
-from aux import str2u
 from aux.service_daemon import CothreadQtService
 
 import pycx4.qcda as cda
@@ -19,9 +18,8 @@ from acc_db.mode_cache import SysCache, ModeCache
 
 
 class EPICSChanAdapter:
-    def __init__(self, name):
+    def __init__(self, name, **kwargs):
         self.chan = catools.camonitor(name, self.new_data, format=catools.FORMAT_TIME)
-
         self.name, self.val, self.time, self.quant = name, 0, 0, 0
 
     def new_data(self, value):
@@ -39,30 +37,28 @@ class EPICSChanAdapter:
 
 
 class ChanFactory:
-    def create_chan(self, protocol, name):
+    def create_chan(self, protocol, name, **kwargs):
         if protocol == 'cx':
-            return cda.DChan(name)
+            return cda.DChan(name, **kwargs)
         if protocol == 'EPICS':
-            return EPICSChanAdapter(name)
+            return EPICSChanAdapter(name, **kwargs)
         return None
 
 
 class ModeDeamon:
     def __init__(self):
         self.db = ModesDB()
-
         ans = self.db.mode_chans()
         self.db_chans = [list(c) for c in ans]
         cf = ChanFactory()
 
-        # dict for faster name-based lookup
-        self.cind = {x[1]: cf.create_chan(x[0], x[1]) for x in self.db_chans}
-        # save ids
-        self.c_ids = {x[1]: x[2] for x in self.db_chans}
+        # dict for faster id-based lookup
+        self.cind = {x[2]: cf.create_chan(x[0], x[1]) for x in self.db_chans}
+        self.name_ind = {x[1]: x[2] for x in self.db_chans}
 
         self.mode_ser = ModesServer()  # message server for accelerator mode control
         self.mode_ser.load.connect(self.loadMode)
-        self.mode_ser.save.connect(self.saveMode)
+        self.mode_ser.save.connect(self.save_mode)
         self.mode_ser.loadMarked.connect(self.loadMarked)
         self.mode_ser.markMode.connect(self.markMode)
         self.mode_ser.walkerLoad.connect(self.walkerLoad)
@@ -81,15 +77,13 @@ class ModeDeamon:
         for k in self.walkers:
             self.walkers[k].done.connect(self.mode_ser.walkerDone)
 
-    def saveMode(self, author, comment):
-        t1 = time.time()
-        cids = self.c_ids
+    def save_mode(self, author, comment):
         cind = self.cind
-        data = [[cids[k], cind[k].time, cind[k].val, cind[k].is_available()] for k in cids]
-        t2 = time.time()
-        mode_id = self.db.save_mode(str2u(author), str2u(comment), data)
-        t3 = time.time()
-        print(t2-t1, t3-t2)
+        data2json = {'columns': ['fullchan_id', 'value', 'time', 'available'],
+                     'doc_type': '1.0',
+                     'data': [[k, cind[k].val, cind[k].time, cind[k].is_available()] for k in cind]}
+        data_json = json.dumps(data2json, ignore_nan=True)
+        mode_id = self.db.save_mode(author, comment, data_json)
         self.mode_ser.saved(mode_id)
 
     def check_syslist(self, syslist):
@@ -100,43 +94,43 @@ class ModeDeamon:
         return True
 
     def applyMode(self, mode_data):
-        # mode_data cols: chan_name, value
+        # mode_data: {id:[value, ...]}
         loaded_count, nochange_count, na_count = 0, 0, 0
-        epics_chans, epics_values = [], []
-
-        for row in mode_data:
-            c_name, c_val = row[0], row[1]
-            chan = self.cind.get(c_name, None)
+        #print(mode_data)
+        for c_id in mode_data:
+            chan = self.cind.get(c_id, None)
             if chan is None:
                 na_count += 1
-                print('unavaliable: ', c_name)
+                print('unavaliable: ', c_id)
                 continue
             # warning !!! it's not always correct
-            if c_val == chan.val:
+            if mode_data[c_id][0] == chan.val:
                 nochange_count += 1
                 continue
-            chan.setValue(c_val)
+            chan.setValue(mode_data[c_id][0])
             loaded_count += 1
         msg = 'loaded %d, nochange %d, unavailiable %d' % (loaded_count, nochange_count, na_count)
         return msg
 
     def load_zeros(self, syslist, a_kinds):
-        namelist = self.sys_cache.cnames(syslist, a_kinds)
-        #zero_mode = [for x in ]
-        print(namelist)
+        print('load_zero requested')
+        cids = self.sys_cache.cids(syslist, a_kinds)
+        zero_mode = {x: [0.0] for x in cids}
+        msg = self.applyMode(zero_mode)
+        self.mode_ser.loaded(msg)
 
     def loadMode(self, mode_id, syslist, types):
         if not self.check_syslist(syslist) or not types:
             return
-        mode_data = self.db.load_mode(mode_id, syslist, types)
-        msg = self.applyMode(mode_data)
+        data = self.db.load_mode(mode_id, syslist, types)
+        msg = self.applyMode(data)
         self.mode_ser.loaded(msg)
 
     def loadMarked(self, mark, syslist, types):
         if not self.check_syslist(syslist) or not types:
             return
-        mode_data = self.mode_caches[mark].extract(syslist, types)
-        msg = self.applyMode(mode_data)
+        data = self.mode_caches[mark].extract(syslist, types)
+        msg = self.applyMode(data)
         self.mode_ser.markedLoaded(mark, msg)
 
     def markMode(self, mode_id, mark, comment, author):
@@ -148,19 +142,22 @@ class ModeDeamon:
     def walkerLoad(self, walkers_path, coefs=None):
         # walkers_path - is a dict with {'walker': [marks] }
         # coefs - the same dictionary with coefs for values
+        #print("walker load: ", walkers_path, coefs)
         for key in walkers_path:
             marks = walkers_path[key]
             if marks[0] is None:
                 del marks[0]
             cname = remag_srv + '.' + key + '.Iset'  # case ?
+            c_id = self.name_ind[cname]
             vs = []
             for m in marks:
-                row = self.mode_caches[m].data[cname]
-                vs.append(row[2])
+                row = self.mode_caches[m].data[c_id]
+                vs.append(row[0])
             if coefs is not None:
                 if coefs[key] is not None:
                     for ind in range(len(vs)):
                         vs[ind] *= coefs[key][ind]
+            #print(key, vs)
             self.walkers[key].run_list(np.array(vs))
 
     def dump_state(self):
