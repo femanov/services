54ab665892df44d186b6f1c0f9757746fe1b3473
femanov <femanov@gmail.com>
2020-09-03 16:20:34 +0700

    version update

--- pi_thermo/rpi_thermo2cx.py
+++ pi_thermo/rpi_thermo2cx.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import pycx4.pycda as cda
 import glob
 import time
 
@@ -7,33 +7,15 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos + 2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
+def fd_ready(ev):
+    lines = ev.file.readlines()
+    print(time.time())
+    print(lines)
 
 f = open(device_file, 'r')
 
+file_ev = cda.FdEvent(f)
 
-while True:
-    print('reading..', time.time())
-    f.seek(0)
-    lines = f.readlines()
-    print('new data', time.time())
-    print(lines)
+file_ev.ready.conneect(fd_ready)
 
-    time.sleep(1)
+cda.main_loop()
